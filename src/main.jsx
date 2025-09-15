d150ecd49f2c4c9c2960e8302c01b8795af4d921
Seckin AKCA <38793483+SeckinBey@users.noreply.github.com>
2025-09-15 12:01:43 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,35 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
+function App() {
+  return (
+    <>
+      <Header />
+      <MovieList />
+    </>
+  );
+}
+
+function Header() {
+  return (
+    <div id="header">
+      <h1>Header</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia modi
+        tenetur porro odio sequi repellendus natus perferendis expedita
+        quibusdam eligendi consequatur atque quo deleniti nobis sunt magni cum,
+        adipisci eveniet.
+      </p>
+    </div>
+  );
+}
+
+function MovieList() {
+  return <h2>MovieList</h2>;
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <h1>Hello world!</h1>
+    <App />
   </StrictMode>
 );
