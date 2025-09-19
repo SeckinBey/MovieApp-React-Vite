4e7b6b523a3d99904c0f75f561db1b6f8975f91b
Seckin AKCA <38793483+SeckinBey@users.noreply.github.com>
2025-09-19 12:20:36 +0300

    added tmdb api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
-
+import Loading from "./components/Loading";
 import Logo from "./components/Logo";
 import SearchForm from "./components/SearchForm";
 import WatchListButton from "./components/WatchListButton";
@@ -10,12 +10,34 @@ import WatchListButton from "./components/WatchListButton";
 import MovieList from "./components/MovieList";
 import WatchList from "./components/WatchList";
 
-import { movie_list } from "./data";
+// import { movie_list } from "./data";
+
+const api_key = "4cd5b9057a539442ab11543bcaa3ded9";
+const page = 1;
+const query = "avengers";
+const language = "en-US";
 
 export default function App() {
-  const [movies, setMovies] = useState(movie_list);
+  const [movies, setMovies] = useState([]);
   const [watchListMovies, setWatchListMovies] = useState([]);
   const [isWatchListOpen, setIsWatchListOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function getMovies() {
+      setLoading(true);
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${query}&page=${page}&language=${language}`
+      );
+
+      const data = await response.json();
+      console.log(data.results);
+
+      setMovies(data.results);
+      setLoading(false);
+    }
+    getMovies();
+  }, []);
 
   function handleAddToWatchList(movie) {
     const isAlreadyInWatchList = watchListMovies
@@ -47,7 +69,11 @@ export default function App() {
           isWatchListOpen={isWatchListOpen}
           onRemoveFromWatchList={handleRemoveFromWatchList}
         />
-        <MovieList movies={movies} onAddToList={handleAddToWatchList} />
+        {loading ? (
+          <Loading />
+        ) : (
+          <MovieList movies={movies} onAddToList={handleAddToWatchList} />
+        )}
       </Main>
       <Footer />
     </>
