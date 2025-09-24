2ccf4d127238bdd8accf3e0f2a9d86c69784308c
Seckin AKCA <38793483+SeckinBey@users.noreply.github.com>
2025-09-24 14:10:31 +0300

    added details with using api

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,34 +1,99 @@
+import { useEffect, useState } from "react";
+import Loading from "./Loading";
+
+const api_key = "4cd5b9057a539442ab11543bcaa3ded9";
+const page = 1;
+const query = "batman";
+const language = "en-US";
+
 export default function MovieDetails({ movieObj, onClose }) {
+  const [loadedMovie, setLoadedMovie] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function getMovieDetails() {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/${movieObj.id}?api_key=${api_key}&language=${language}&append_to_response=credits`
+        );
+
+        if (!response.ok) {
+          throw new Error("Hata oluştu");
+        }
+
+        const data = await response.json();
+
+        if (data) {
+          setLoadedMovie(data);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+      setLoading(false);
+    }
+
+    getMovieDetails();
+  }, [movieObj.id]);
+
   return (
-    <div className="movie-details">
+    <div className="my-3">
       <div className="card">
         <div className="card-header d-flex justify-content-between align-items-center">
-          <h2 className="h5 mb-0">{movieObj.title}</h2>
-          <button className="btn btn-sm btn-danger" onClick={() => onClose()}>
+          <h2 className="title h5 mb-0">Movie Details</h2>
+          <button className="btn btn-danger btn-sm" onClick={() => onClose()}>
             Close
           </button>
         </div>
         <div className="card-body">
           <div className="row">
-            <div className="col-md-3">
-              <img
-                src={
-                  "https://image.tmdb.org/t/p/original/" + movieObj.poster_path
-                }
-                alt=""
-                className="img-fluid"
-              />
-            </div>
-            <div className="col-md-9">
-              <h3>{movieObj.title}</h3>
-              <p>{movieObj.overview}</p>
-              <p>
-                <strong>Release Date:</strong> {movieObj.release_date}
-              </p>
-              <p>
-                <strong>Rating:</strong> {movieObj.vote_average} / 10
-              </p>
-            </div>
+            <img
+              src={
+                "https://image.tmdb.org/t/p/original/" + movieObj.backdrop_path
+              }
+              alt=""
+              className="img-fluid"
+            />
+            <h3>{movieObj.title}</h3>
+            <p>{movieObj.overview}</p>
+            <p>Release Date: {movieObj.release_date}</p>
+            <p>Rating: {movieObj.vote_average}</p>
+            {loading && <Loading />}
+            {loadedMovie && (
+              <>
+                <p>Time: {loadedMovie.runtime}</p>
+                <p>Country: {loadedMovie.production_countries[0].name}</p>
+                <p>Yapımcı: {loadedMovie.production_companies[0].name}</p>
+                <p>Yönetmen: {loadedMovie.credits.crew[0].name}</p>
+                <p>Senarist: {loadedMovie.credits.crew[1].name}</p>
+                <p>Türler:</p>
+                <ul className="list-unstyled d-flex flex-wrap">
+                  {loadedMovie.genres.map((genre) => (
+                    <li key={genre.id} className="badge bg-primary me-2 mb-2">
+                      {genre.name}
+                    </li>
+                  ))}
+                </ul>
+                <div className="card card-body">
+                  <p>Oyuncular:</p>
+                  <div className="row">
+                    {loadedMovie.credits.cast.slice(0, 12).map((actor) => (
+                      <div className="col-md-2" key={actor.id}>
+                        <img
+                          src={
+                            "https://image.tmdb.org/t/p/original/" +
+                            actor.profile_path
+                          }
+                          alt={actor.name}
+                          className="img-fluid"
+                        />
+                        <p>{actor.name}</p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
